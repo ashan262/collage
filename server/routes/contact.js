5e0e49a4e3588eda2d19abfae8c4975f3b548221
ashan-khan <khan.ashan.khan1234@gmail.com>
2025-10-16 18:47:16 +0500

    contact message responsive

--- server/routes/contact.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createContact } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-// Public routes
-router.post("/", createContact);
-
-export default router;
